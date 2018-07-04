de856fab7fe13fb7c45a1d0a2ed77f7fe6fc909d
GabrielFurtado <gabriel.f.salarini@gmail.com>
2018-07-04 15:38:33 -0300

    simple js project

---
+++ questions.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What's your first name?");
+var lastName= prompt("what's your last name?");
+var age= prompt("What's your age?");
+var fullName= firstName + " " + lastName
+console.log("Welcome " + fullName);
+console.log(" You are " + age + " years old");
